f396559d7aaf1e40b1a829fc36de3302970e1278
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-05-10 21:30:43 -0600

    ⭐FIN Github User Card

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
 import GithubCard from './components/GithubCard.vue';
 </script>
-
 <template>
   <div class="">
-    <GithubCard username="Yampyyii" />
+    <GithubCard username="JazmariGonzalez"/>
     <GithubCard username="rivalcoba"/>
     <GithubCard username="BrunoPCH"/>
   </div>
 </template>
-
-<style scoped></style>
--- src/components/GithubCard.vue
+++ src/components/GithubCard.vue
@@ -1,23 +1,38 @@
 <script setup>
+import { ref } from 'vue';
+const user = ref(null);
+const props = defineProps({
+  username: {
+    type: String,
+    required: true,
+  },
+});
+// fetching data
+fetch(`https://api.github.com/users/${props.username}`)
+  .then(async (res) => {
+    user.value = await res.json();
+  }).catch((err) => {
+    console.log(err);
+  });
 </script>
 
 <template>
-  <div class="card card-side bg-base-100 shadow-xl">
+  <div v-if="user" class="m-5 card card-side bg-base-100 shadow-xl">
     <figure>
       <img
-        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-        alt="Movie"
+        :src="user.avatar_url"
+        alt="Avatar Image"
       />
     </figure>
     <div class="card-body">
-      <h2 class="card-title">New movie is released!</h2>
-      <p>Click the button to watch on Jetflix app.</p>
+      <h2 class="card-title">{{ user.name }}</h2>
+      <p>
+        <strong>Seguidores:</strong> {{ user.followers }}
+        <strong>Siguiendo:</strong> {{ user.following }}
+      </p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary">Watch</button>
+        <a :href="user.html_url" class="btn btn-primary">Ver Perfil</a>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-</style>
